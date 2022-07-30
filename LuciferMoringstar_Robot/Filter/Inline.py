eefa5596707364b2b26dcb33ad576e26c6c7c938
ginsaaan <96005230+ginsaaan@users.noreply.github.com>
2022-07-30 11:21:21 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -89,7 +89,7 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Master', url=f'{TUTORIAL}')
+        InlineKeyboardButton('Master', url=f'https://t.me/GINSAAAN')
         ],[
         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
         ]]
