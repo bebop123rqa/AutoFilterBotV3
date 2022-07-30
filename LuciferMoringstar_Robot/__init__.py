979d5b503d4bf1e185f7949ca751c3a029d72583
bebop123rqa <108179926+bebop123rqa@users.noreply.github.com>
2022-07-30 11:47:00 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -15,15 +15,9 @@ Basic Commads
 
 /start :- check am Alive
 
-Bot Owner Only
-/broadcast Replay Any Message or Media
-/stats user Status
-/ban_user  click ban_user more info
-/unban_user click unban_user more info
-/banned_users Banned User Details
-/total How Many Files Added In Database
-/logger  Get Logs
-/delete Delete File From Database
+Video Tutorial : [Click here](https://rb.gy/zw1tdk)
+
+Telegraph Tutorial : [Click here](http://3c5.com/eDYSv)
 
 """
 
