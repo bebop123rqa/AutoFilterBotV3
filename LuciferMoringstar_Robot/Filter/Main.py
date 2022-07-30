9adf840ce6b6f175ad21e0d8a69496813631bd9d
bebop123rqa <108179926+bebop123rqa@users.noreply.github.com>
2022-07-30 11:27:56 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -308,7 +308,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('Fiction-recs', url=f'https://t.me/fivtiontvgroup')
+                        InlineKeyboardButton('Fiction-recs', url=f'https://t.me/fictiontvgroup')
                     ]
                     ]
                 
